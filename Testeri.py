17a9c2edca85bc2cf0554f07237f917f3fec1f68
Markku <markku.seppala@gmail.com>
2025-02-25 17:53:29 +0200

    Muutos 1753

--- Testeri.py
+++ Testeri.py
@@ -1,4 +1,18 @@
 from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Sovellus toimii Railwayssa!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Railway määrittää portin ympäristömuuttujassa
+    app.run(host="0.0.0.0", port=port)  # Kuuntelee kaikista osoitteista
+
+
+
 
 import sys
 sys.path.append("C:/Talobot")  # Varmista, että polku on oikea
@@ -50,8 +64,8 @@ def index():
 if __name__ == "__main__":
     app.run(debug=True)
 
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+#import os
+#if __name__ == "__main__":
+#    port = int(os.environ.get("PORT", 5000))
+#    app.run(host="0.0.0.0", port=port, debug=True)
 #
