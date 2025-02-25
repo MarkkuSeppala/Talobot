b74bb3411cfb88056d026ac5b36daeb61da97bb6
Markku <markku.seppala@gmail.com>
2025-02-25 17:21:28 +0200

    Muutos 1721

--- sie.py
+++ sie.py
@@ -151,6 +151,6 @@ def suorita_lohko5():
     tulosta_viesti("lohko5 suoritettu", kellonaika)
     print(response.text)
     return kellonaika
-#
+#testi kommentti
 # **Tarkistetaan, että tulosta_viesti on määritelty**
 tulosta_viesti = print
