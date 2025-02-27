2a11abbec4f41dac69a4b5f64d384f5560ff089e
Markku <markku.seppala@gmail.com>
2025-02-27 19:22:10 +0200

    1922

--- yhdistetaan_ikkunat.py
+++ yhdistetaan_ikkunat.py
@@ -1,3 +1,4 @@
+'''
 import json
 
 def yhdista_ikkunat(sievitalo_tiedosto, kastelli_tiedosto, output_tiedosto):
@@ -58,16 +59,69 @@ def yhdista_ikkunat(sievitalo_tiedosto, kastelli_tiedosto, output_tiedosto):
     print("Yhdistetyt ikkunat tallennettu tiedostoon:", output_tiedosto)
 
 # **Uusi metodi, joka kutsuu yhdistämistoimintoa oikeilla tiedostopolkuilla**
-'''
-def suorita_yhdistaminen():
-    sievitalo_tiedosto = "data/s/ikkuna_json_2.txt"
-    kastelli_tiedosto = "data/k/ikkuna_json_2.txt"
-    output_tiedosto = "data/yhd/ikkunat_yhdessa.txt"
 
-    print("Aloitetaan Sievitalon ja Kastellin ikkunoiden yhdistäminen...")
-    yhdista_ikkunat(sievitalo_tiedosto, kastelli_tiedosto, output_tiedosto)
-    print("Yhdistäminen valmis!")
-
-# **Kutsutaan yhdistämismetodia**
-suorita_yhdistaminen()
 '''
+import json
+
+def yhdista_ikkunat(sievitalo_tiedosto, kastelli_tiedosto, output_tiedosto):
+    # Ladataan JSON-tiedot
+    with open(sievitalo_tiedosto, "r", encoding="utf-8") as tiedosto:
+        sievitalo_data = json.load(tiedosto)
+
+    with open(kastelli_tiedosto, "r", encoding="utf-8") as tiedosto:
+        kastelli_data = json.load(tiedosto)
+
+    yhdistetyt_ikkunat = {}
+
+    # **Käsitellään Sievitalon ikkunat**
+    for item in sievitalo_data:
+        koko = item.get("mm_koko", "Tuntematon")  # Varmistetaan, että avain on olemassa
+        yhdistetyt_ikkunat[koko] = {
+            "Sievitalo_mm_koko": koko,
+            "Kastellin_pyoristetty_koko": None,
+            "Sievitalo_turvalasi": item.get("turvalasi", None),
+            "Kastelli_turvalasi": None,
+            "Sievitalo_välikarmi": item.get("välikarmi", None),
+            "Kastelli_välikarmi": None,
+            "Sievitalo_sälekaihtimet": item.get("sälekaihtimet", None),
+            "Kastelli_sälekaihtimet": None
+        }
+
+    # **Käsitellään Kastellin ikkunat**
+    for item in kastelli_data:
+        koko = item.get("pyoristetty_koko", "Tuntematon")  # Varmistetaan, että avain on olemassa
+        if koko in yhdistetyt_ikkunat:
+            # Täydennetään olemassa olevaa riviä
+            yhdistetyt_ikkunat[koko]["Kastellin_pyoristetty_koko"] = koko
+            yhdistetyt_ikkunat[koko]["Kastelli_turvalasi"] = item.get("turvalasi", None)
+            yhdistetyt_ikkunat[koko]["Kastelli_välikarmi"] = item.get("välikarmi", None)
+            yhdistetyt_ikkunat[koko]["Kastelli_sälekaihtimet"] = item.get("sälekaihtimet", None)
+        else:
+            # Lisätään uusi rivi Kastellin ikkunalle
+            yhdistetyt_ikkunat[koko] = {
+                "Sievitalo_mm_koko": None,
+                "Kastellin_pyoristetty_koko": koko,
+                "Sievitalo_turvalasi": None,
+                "Kastelli_turvalasi": item.get("turvalasi", None),
+                "Sievitalo_välikarmi": None,
+                "Kastelli_välikarmi": item.get("välikarmi", None),
+                "Sievitalo_sälekaihtimet": None,
+                "Kastelli_sälekaihtimet": item.get("sälekaihtimet", None)
+            }
+
+    # **Muutetaan sanakirja listaksi ja lajitellaan leveyden mukaan**
+    try:
+        output_json = sorted(
+            yhdistetyt_ikkunat.values(),
+            key=lambda x: int(x["Sievitalo_mm_koko"].split("x")[0]) if x["Sievitalo_mm_koko"] else int(x["Kastellin_pyoristetty_koko"].split("x")[0])
+        )
+    except Exception as e:
+        print("Virhe lajittelussa:", str(e))
+        output_json = list(yhdistetyt_ikkunat.values())
+
+    # Tallennetaan yhdistetyt ikkunat tiedostoon
+    with open(output_tiedosto, "w", encoding="utf-8") as tiedosto:
+        json.dump(output_json, tiedosto, ensure_ascii=False, indent=4)
+
+    print("Yhdistetyt ikkunat tallennettu tiedostoon:", output_tiedosto)
+
