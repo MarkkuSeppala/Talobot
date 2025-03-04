34f74073cb664f06db94baef7fdc4d83761b6132
Markku <markku.seppala@gmail.com>
2025-03-04 18:14:57 +0200

    Lisätty väli- ja ulko-ovien kyselyt sekä kyselyjen tulostukset selaimeen.

--- s_flask.py
+++ s_flask.py
@@ -10,20 +10,28 @@ import os
 from s_toimitussisalto_tekstiksi_ja_clean import muuta_tekstiksi, clean_text, poista_sanat_tekstista
 from s_ikkuna_API_kyselyt_tulostus_to_JSON import api_kysely_poimi_ikkunatiedot, api_ryhmittele_valitut_ikkunatiedot_json_muotoon, jokainen_ikkuna_omalle_riveille_ja_koko_millimetreiksi
 from s_valiovet_API_kyselyt_tulostus_to_JSON import api_kysely_poimi_valiovitiedot, api_kysely_anna_valiovimallit
-from s_ulko_ovi_API_kyselyt_tulostus_to_JSON import api_kysely_poimi_ulko_ovitiedot
-from file_handler import lue_txt_tiedosto, kirjoita_txt_tiedosto
+from s_ulko_ovi_API_kyselyt_tulostus_to_JSON import api_kysely_poimi_ulko_ovitiedot, api_ryhmittele_valitut_ulko_ovitiedot_json_muotoon, api_poistaa_valitut_sanat_ulko_ovitiedoista_json_muotoon
+from file_handler import lue_txt_tiedosto, lue_json_tiedosto, kirjoita_txt_tiedosto
 from datetime import datetime 
+import json
 
 app = Flask(__name__)
 
 
-# 🔹 flask  "ulko-ovitiedot" 
+# 🔹 Ulko-ovet. Api-kyselyt ja tulostus selaimeen. 
 @app.route("/suodata_ulko_ovitiedot", methods=["GET"])
 def suodata_ulko_ovitiedot():
     api_kysely_poimi_ulko_ovitiedot()
-    #api_kysely_anna_valiovimallit()
-    sisalto2 = lue_txt_tiedosto("data/s/valiovityypit.txt")
-    return Response(sisalto2, mimetype="text/plain")
+    api_ryhmittele_valitut_ulko_ovitiedot_json_muotoon()
+    api_poistaa_valitut_sanat_ulko_ovitiedoista_json_muotoon()
+    ulko_ovitiedot_str_1 = lue_txt_tiedosto("data/s/ulko_ovi_tiedot_kokonaisuudessa.txt")
+    #ulko_ovitiedot_str_1 = json.dumps(ulko_ovitiedot, ensure_ascii=False, indent=4)
+    ulko_ovitiedot = lue_json_tiedosto("data/s/ulko_ovi_tiedot.json")
+    ulko_ovitiedot_str_2 = json.dumps(ulko_ovitiedot, ensure_ascii=False, indent=4)
+    ulko_ovitiedot = lue_json_tiedosto("data/s/ulko_ovi_tiedot_2.json")
+    ulko_ovitiedot_str_3 = json.dumps(ulko_ovitiedot, ensure_ascii=False, indent=4)
+    return Response(ulko_ovitiedot_str_1+"\n\n\n\n\n\n"+ulko_ovitiedot_str_2+"\n\n\n\n\n\n"+ulko_ovitiedot_str_3, mimetype="application/json")
+        
 
 # 🔹 Uusi funktio, jota painike "Suodata väliovitiedot" kutsuu
 @app.route("/suodata_valiovitiedot", methods=["GET"])
@@ -73,8 +81,10 @@ def index():
         </form>
         <br><br><br><h4>Valitse sitten haluatko suodattaa ikkunatiedot...</h2>
         {"<button onclick=\"window.location.href='/suodata_ikkunatiedot'\">Suodata ikkunatiedot</button>" if painike_nayta else ""}
+        
         <br><br><br><h4>vai välioviteidot...<br></h2><br>
         {"<button onclick=\"window.location.href='/suodata_valiovitiedot'\">Suodata väliovitiedot</button>" if painike_nayta else ""}
+        
         <br><br><br><h4>vai ulko-oviteidot...<br></h2><br>
         {"<button onclick=\"window.location.href='/suodata_ulko_ovitiedot'\">Suodata ulko-ovitiedot</button>" if painike_nayta else ""}
         
